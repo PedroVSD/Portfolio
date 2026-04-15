3179432e038f4db929e21410b8772c29db6758d6
PedroVSD-linux <pedrovsd00@gmail.com>
2026-04-15 13:09:17 -0300

    melhorando o mobile

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-const toggleBtn = document.getElementById('theme-toggle')
-const htmlElement = document.documentElement
+const toggleBtn = document.getElementById("theme-toggle");
+const htmlElement = document.documentElement;
 
-const savedTheme = localStorage.getItem('theme')
+const savedTheme = localStorage.getItem("theme");
 if (savedTheme) {
-    htmlElement.setAttribute('data-theme', savedTheme)
+  htmlElement.setAttribute("data-theme", savedTheme);
 }
 
-toggleBtn.addEventListener('click', () => {
-    const currentTheme = htmlElement.getAttribute('data-theme')
+toggleBtn.addEventListener("click", () => {
+  const currentTheme = htmlElement.getAttribute("data-theme");
 
-    if (currentTheme === 'dark') {
-        htmlElement.removeAttribute('data-theme')
-        localStorage.setItem('theme', 'light')
-    } else {
-        htmlElement.setAttribute('data-theme', 'dark')
-        localStorage.setItem('theme', 'dark')
-    }
-})
+  if (currentTheme === "dark") {
+    htmlElement.removeAttribute("data-theme");
+    localStorage.setItem("theme", "light");
+  } else {
+    htmlElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  }
+});
+
+// Lógica do Menu Hambúrguer
+const hamburgerBtn = document.getElementById("hamburger-btn");
+const navLinks = document.querySelector(".nav-links");
+
+hamburgerBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+// Fecha o menu automaticamente quando o usuário clica em um link
+const links = document.querySelectorAll(".nav-links a");
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
+});
