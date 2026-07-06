fdbfca774b35643d6b8277e0890e6c00c5b11351
PedroVSD-linux <pedrovsd00@gmail.com>
2026-07-05 22:33:06 -0300

    Arrumando o visual do botão de tradução. E adicionando os json de tradução

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const toggleBtn = document.getElementById("theme-toggle");
 const htmlElement = document.documentElement;
-
 const savedTheme = localStorage.getItem("theme");
+const hamburgerBtn = document.getElementById("hamburger-btn");
+const navLinks = document.querySelector(".nav-links");
+const idiomas = ["pt", "en"];
+
 if (savedTheme) {
   htmlElement.setAttribute("data-theme", savedTheme);
 }
@@ -19,9 +22,6 @@ toggleBtn.addEventListener("click", () => {
 });
 
 // Lógica do Menu Hambúrguer
-const hamburgerBtn = document.getElementById("hamburger-btn");
-const navLinks = document.querySelector(".nav-links");
-
 hamburgerBtn.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
@@ -33,3 +33,53 @@ links.forEach((link) => {
     navLinks.classList.remove("active");
   });
 });
+
+//Tradução
+const supportedLanguages = ["pt", "en"];
+
+function detectBrowserLanguage() {
+  const browserLanguage = navigator.language.toLowerCase();
+
+  return browserLanguage.startsWith("pt") ? "pt" : "en";
+}
+
+function getCurrentLanguage() {
+  const savedLanguage = localStorage.getItem("language");
+
+  if (savedLanguage && supportedLanguages.includes(savedLanguage)) {
+    return savedLanguage;
+  }
+
+  return detectBrowserLanguage();
+}
+
+async function loadLanguage(language) {
+  const response = await fetch(`/locales/${language}.json`);
+
+  const translations = await response.json();
+
+  document.querySelectorAll("[data-i18n]").forEach((element) => {
+    const key = element.dataset.i18n;
+
+    const value = key
+      .split(".")
+      .reduce((obj, item) => obj?.[item], translations);
+
+    if (value) {
+      element.textContent = value;
+    }
+  });
+
+  document.documentElement.lang = language === "pt" ? "pt-BR" : "en";
+
+  localStorage.setItem("language", language);
+}
+
+function toggleLanguage() {
+  const atual = localStorage.getItem("language") || getAtutalLang();
+
+  const proximo = atual === "pt" ? "en" : "pt";
+  loadLanguage(proximo);
+}
+
+loadLanguage(getAtutalLang());
